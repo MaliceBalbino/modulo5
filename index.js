88ea60952c6a042f7e39cd3cbb271e54daa61220
MaliceBalbino <113525971+MaliceBalbino@users.noreply.github.com>
2023-02-03 12:01:30 -0300

    Adicionando a primeira versão

---
+++ index.js
@@ -0,0 +1,10 @@
+readlineSync= require ('readline-sync');
+const palavra = []
+const input = ""
+
+while (input != "sair") {
+   palavra.push(input)
+    
+input= readlineSync.question('digite uma propriedade css');
+
+};
